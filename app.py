3aa89ed70e023e198023be7fb2b09267093ff495
Luz <estelarodrigoluz@gmail.com>
2025-10-17 12:24:17 +0000

    feat: adiciona estrutura base da aplicação principal

---
+++ app.py
@@ -0,0 +1,8 @@
+"""
+Sistema de Gestão de Reservas de Hotel
+Aplicação principal com menu interativo
+"""
+
+from clientes import adicionar_cliente, listar_clientes, clientes
+from quartos import adicionar_quarto, listar_quartos, atualizar_status_quarto, quartos
+from reservas import criar_reserva, cancelar_reserva, listar_reservas, reservas
